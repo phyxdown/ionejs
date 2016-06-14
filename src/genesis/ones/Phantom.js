3a86dd5839ea1e6348857c888a14a0f870dcee33
phyxdown <264162213@qq.com>
2016-06-14 21:22:36 +0800

    add Phantom

--- src/genesis/ones/Phantom.js
+++ src/genesis/ones/Phantom.js
@@ -1,49 +1,45 @@
-var inherits = require('../../utils/inherits');
-var One = require('../One');
+var definer = require('../../helpers/definer');
+var _ = require('underscore');
 
-var Phantom = function(options) {
-    One.apply(this, arguments);
-    this._alias = 'ionejs.Phantom';
-    this.mReset();
-};
+definer.defineOne({
+    afterCreate: function() {
+        this.mReset();
+    },
 
-var p = inherits(Phantom, One);
+    set: function(one) {
+        if (one instanceof One || one == null) {
+            this._origin = one;
+        } else {
+            console.log("#phantom.set#", "illegal params.");
+        }
+    },
 
-p.set = function(one) {
-    if (one instanceof One || one == null) {
-        this._origin = one;
-    } else {
-        console.log("#phantom.set#", "illegal params.");
+    mTrz: function(matrix){
+        this.mM = matrix;
+    },
+
+    mTsl: function(x, y){
+        this.mX = x;
+        this.mY = y;
+    },
+
+    mReset: function(){
+        this.mM = this.getAbsoluteMatrix();
+        this.mX = 0;
+        this.mY = 0;
+    },
+
+    draw: function(context) {
+        var m = this.mM;
+        var x = this.mX;
+        var y = this.mY;
+        context.save();
+        context.translate(x, y);
+        context.transform(m.a, m.b, m.c, m.d, m.x, m.y);
+
+        if (this._origin)
+            this._origin._draw(context);
+        
+        context.restore();
     }
-};
-
-p.mTrz = function(matrix){
-    this.mM = matrix;
-};
-
-p.mTsl = function(x, y){
-    this.mX = x;
-    this.mY = y;
-};
-
-p.mReset = function(){
-    this.mM = this.getAbsoluteMatrix();
-    this.mX = 0;
-    this.mY = 0;
-};
-
-p.draw = function(context) {
-    var m = this.mM;
-    var x = this.mX;
-    var y = this.mY;
-    context.save();
-    context.translate(x, y);
-    context.transform(m.a, m.b, m.c, m.d, m.x, m.y);
-
-    if (this._origin)
-        this._origin._draw(context);
-    
-    context.restore();
-};
-
-module.exports = Phantom;
+}, 'ionejs.Phantom');
--- src/genesis/ones/index.js
+++ src/genesis/ones/index.js
@@ -1,5 +1,7 @@
 var ones = {};
     ones.Stage = require('./Stage');
     ones.Writer = require('./Writer');
+    ones.Cliper = require('./Cliper');
+    ones.Painter = require('./Painter');
 
 module.exports = ones;
