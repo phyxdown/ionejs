8eacdf6bfea2d8357bb6ceef5c9a9b3e22f51d25
phyxdown <264162213@qq.com>
2017-06-22 15:02:04 +0800

    bug fix

--- src/helpers/definer.js
+++ src/helpers/definer.js
@@ -36,10 +36,23 @@ p.define = function(optionsOrUpdate, superConstruct, alias){
     //if(typeof alias != 'string')
     //    throw new Error('Alias is not string but ' + typeof alias);
 
-    function parseMethod(script) {
+    function parseOneMethod(script) {
+       var pref = "var I = this;var S = I.state;var P = I.getParent();var PS = P.state;var L = I.getLeader();var GS = I.getGroupState();"
+       return new Function(pref + script);
+    }
+
+    function parseActionMethod(script) {
        var pref = "var A = this;var I = A.one;var S = I.state;var P = I.getParent();var PS = P.state;var L = I.getLeader();var GS = I.getGroupState();"
        return new Function(pref + script);
     }
+
+    var baseConstruct = construct;
+    while(baseConstruct._super != undefined)
+        baseConstruct = baseConstruct._super;
+
+    var parseMethod;
+    if(baseConstruct == One) parseMethod = parseOneMethod;
+    if(baseConstruct == Action) parseMethod = parseActionMethod;
 
     var options;
     if(typeof optionsOrUpdate == 'function') options = { update: optionsOrUpdate }
@@ -85,9 +98,7 @@ p.define = function(optionsOrUpdate, superConstruct, alias){
     for (var i in methods) {
         p[i] = methods[i];
     }
-    var baseConstruct = construct;
-    while(baseConstruct._super != undefined)
-        baseConstruct = baseConstruct._super;
+
     if(baseConstruct == One) register.Ones[alias] = construct;
     if(baseConstruct == Action) register.Actions[alias] = construct;
     return construct;
